cb48e98f215ce6962b8c716e63f6d12e588dc8cc
mireiaGomariz <mireiapgomariz@gmail.com>
2020-01-31 11:53:28 +0100

    Api integration firsts steps

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+
+  constructor(props) {
+
+    //get all the propieties of the content
+
+    super(props);
+    this.state = {
+      items: [],
+      isLoaded: false,
+    }
+  }
+
+  componentDidMount() {
+
+    fetch('https://rickandmortyapi.com/api')
+      .then(res => res.json())
+      .then(json => {
+
+        // set the state, manipulate that items
+
+          this.setState ({
+            isLoaded: true,
+            items: json
+          })
+      });
+
+  }
+
+  render() {
+
+    var { isLoaded, items } = this.state;
+
+    if (!isLoaded) {
+      return <div> Loading...</div>;
+
+    }else {
+      return(
+        <div className="App">
+            Data has been loaded
+        </div>
+      );
+    }
+
+  }
 }
+
 
 export default App;
