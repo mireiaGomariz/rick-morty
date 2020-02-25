9f49c608ea5a9eb02ecb874c68a4a7983edf74b8
mireiaGomariz <mireiapgomariz@gmail.com>
2020-02-25 19:30:55 +0100

    SetInterval instructions1

--- src/Components/Games/Game1/Instructions/instructions1.js
+++ src/Components/Games/Game1/Instructions/instructions1.js
@@ -1,36 +1,51 @@
 import React, { Component } from 'react';
 import  './instructions1.css';
+import image1 from './fondo-run-dialogo.png';
+import image2 from './fondo-run-dialogo2.png';
 import {Link} from 'react-router-dom';
-
+import image3 from './fondo-run-.png';
 
 
 
 class Instructions1 extends Component {
   constructor(props) {
     super(props)
-    this.state = {
-      showFirst: true
+    this.state={
+      intervalId: ''
     }
 
   }
 
   componentDidMount(){
+    let index = 0
+    let images = [image3, image1, image2];
     let self = this
-    setTimeout(function(){
-      // var url = images;
-      // var background = document.getElementById('image').style.backgroundImage = 'url('+image2+')'
-      // var animation = document.getElementById('instructions1').className = 'instructions2'
-      self.setState({showFirst: false})
+    let intervalId = setInterval(function(){
 
-    },5000);
+      // images[index++ % images.length]
+      console.log(document.getElementById('image'));
+        document.getElementById('image').style.backgroundImage = 'url('+images[index]+')'
+        index = index + 1
+      if (index===3) {
+        stopCountDown()
+      }
+      // var url = images;
+      // var animation = document.getElementById('instructions1').className = 'instructions2'
+    }, 2000);
+
+    const stopCountDown = () => {
+      clearInterval(intervalId); // Not working
+    }
+
   }
+
+
 
 
   render() {
     return (
-      <div>
-      {this.state.showFirst
-        ?
+
+
         <div className="instructions1" id="instructions1">
           <div className="image" id="image">
                 <h1 className="hinst-1"> {`It's Alive? `}</h1>
@@ -39,18 +54,8 @@ class Instructions1 extends Component {
             <button href="/game1"><Link to="/game1">Game 1</Link></button>
           </div>
         </div>
-        :
-        <div className="instructions2" id="instructions2">
-          <div className="image2" id="image">
-                <h1 className="hinst-1"> {`It's Alive? `}</h1>
-          </div>
-          <div className="">
-            <button href="/game1"><Link to="/game1">Game 1</Link></button>
-          </div>
-        </div>
 
-      }
-      </div>
+
 
     )
   }
