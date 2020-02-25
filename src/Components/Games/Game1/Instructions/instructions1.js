21eb49511b0e97f7721d3b5b0f2889b40e1fa4bd
mireiaGomariz <mireiapgomariz@gmail.com>
2020-02-25 14:06:20 +0100

    instructions add fadeIn and sound

--- src/Components/Games/Game1/Instructions/instructions1.js
+++ src/Components/Games/Game1/Instructions/instructions1.js
@@ -6,33 +6,54 @@ import {Link} from 'react-router-dom';
 
 
 
-
 class Instructions1 extends Component {
   constructor(props) {
     super(props)
+    this.state = {
+      showFirst: true
+    }
 
   }
 
   componentDidMount(){
+    let self = this
     let images = [image1, image2];
     setTimeout(function(){
-      var url = images;
-      var background = document.getElementById('image').style.backgroundImage = 'url('+image2+')';
-    },5000);
+      // var url = images;
+      // var background = document.getElementById('image').style.backgroundImage = 'url('+image2+')'
+      // var animation = document.getElementById('instructions1').className = 'instructions2'
+      self.setState({showFirst: false})
 
+    },5000);
   }
 
 
   render() {
     return (
       <div>
-        <div className="image" id="image">
-              <h1 className="hinst-1"> {`It's Alive? `}</h1>
+      {this.state.showFirst
+        ?
+        <div className="instructions1" id="instructions1">
+          <div className="image" id="image">
+                <h1 className="hinst-1"> {`It's Alive? `}</h1>
+          </div>
+          <div className="">
+            <button href="/game1"><Link to="/game1">Game 1</Link></button>
+          </div>
         </div>
-        <div className="">
-          <button href="/game1"><Link to="/game1">Game 1</Link></button>
+        :
+        <div className="instructions2" id="instructions2">
+          <div className="image2" id="image">
+                <h1 className="hinst-1"> {`It's Alive? `}</h1>
+          </div>
+          <div className="">
+            <button href="/game1"><Link to="/game1">Game 1</Link></button>
+          </div>
         </div>
+
+      }
       </div>
+
     )
   }
 }
